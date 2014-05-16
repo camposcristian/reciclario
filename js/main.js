4add38c4fbbcfa32ac5d7cc57dd77c0d7198241d
Mariano <xsin.crueldadx@gmail.com>
2014-05-16 16:52:24 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-﻿
+﻿/*
+var cache = {};
+	
+	$("#buscar_track").autocomplete({
+		minLength : 2,
+		source : function(request, response) {
+			var term = request.term;
+			
+			if ( term in cache) {
+				response(cache[term]);
+				return;
+			}
+
+			$.getJSON("https://spotifyapps.contenidos-digitales.com/axe/app.php/search/track/", request, function(data, status, xhr) {
+				cache[term] = data;
+				response(data);
+			});
+		},
+		select : function(event, ui) {
+			$('#seltrack').val(ui.item.id);
+		}
+	});
+*/
