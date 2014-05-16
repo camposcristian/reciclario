be56f6e4b79ce6f5e1fd6dbc9e653c2b1fd7f3a3
Cristian Campos <cristiancampos@weggu.com>
2014-05-16 16:49:38 -0300

    Autocomplete no funcional

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-﻿
+﻿$(document).on("ready", function () {
+    $.get("/fakes/residuos.json").success(function (residuos) {
+        $( "#residuo" ).autocomplete({
+            minLength: 2,
+            source: residuos,
+            focus: function( event, ui ) {
+                $("#residuo").val(ui.item.post_title);
+                return false;
+            },
+            select: function( event, ui ) {
+                $( "#residuo" ).val( ui.item.post_title );
+                $( "#residuo-id" ).val( ui.item.value );
+                $( "#residuos-description" ).html( ui.item.post_type );
+                return false;
+            }
+        })
+        .data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+            return $( "<li>" )
+              .append("<a>" + item.post_title + "<br>" + item.post_type + "</a>")
+              .appendTo( ul );
+        };
+    });
+});
