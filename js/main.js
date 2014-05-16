d393db5c37b3b6b2e9c3243557f3cdfbc04823f7
Mariano <xsin.crueldadx@gmail.com>
2014-05-16 17:13:58 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,27 +1,4 @@
-/*
-var cache = {};
-	
-	$("#buscar_track").autocomplete({
-		minLength : 2,
-		source : function(request, response) {
-			var term = request.term;
-			
-			if ( term in cache) {
-				response(cache[term]);
-				return;
-			}
-
-			$.getJSON("https://spotifyapps.contenidos-digitales.com/axe/app.php/search/track/", request, function(data, status, xhr) {
-				cache[term] = data;
-				response(data);
-			});
-		},
-		select : function(event, ui) {
-			$('#seltrack').val(ui.item.id);
-		}
-	});
-*/
-﻿$(document).on("ready", function () {
+$(document).on("ready", function () {
     $.get("/fakes/residuos.json").success(function (residuos) {
         $( "#residuo" ).autocomplete({
             minLength: 2,
@@ -44,4 +21,3 @@ var cache = {};
         };
     });
 });
->>>>>>> 10738db71579c6f927a3c57058666e7c9ad8796a
