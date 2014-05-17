825ff1c7f07ff6b73f5454dab52153efbdc3cf87
Cristian Campos <cristiancampos@weggu.com>
2014-05-17 15:33:15 -0300

    ID Funcionandooo!

--- js/main.js
+++ js/main.js
@@ -1,32 +1,24 @@
 $(document).on("ready", function () {
-    
     var residuos = new Bloodhound({
-        datumTokenizer: function (d) {
-            return d;
-        },
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
         queryTokenizer: Bloodhound.tokenizers.whitespace,
         limit: 5,
         prefetch: {
             url: 'fakes/residuos.json',
             filter: function (list) {
-                return $.map(list, function (residuo) { return { name: residuo.label, id: residuo.ID, reciclable: residuo.post_type }; });
+                return $.map(list, function (residuo) { return { name: residuo.label, id: residuo.ID }; });
             }
         }
     });
-
     residuos.initialize();
     $('#residuo').typeahead(null, {
         name: 'residuos',
-        valueKey: 'reciclable',
         displayKey: 'name',
         source: residuos.ttAdapter()
     }).bind('typeahead:selected', function (obj, selected, name) {
-        residuos.get('a', function (suggestions) {
-            $.each(suggestions, function (index, item) {
-                console.log(item);
-            });
-        });
+        id = selected.id;
     });
+
 
     $("#form_residuos").submit(function( event ) {
       event.preventDefault();
@@ -41,7 +33,7 @@ $(document).on("ready", function () {
         $("#main").toggle("fade");
         $("#residuo_description").toggle("fade");
 
-      }
+      };
 
     });
 
